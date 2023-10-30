e5032598e67c265079a9d7765361e58b590f94b2
Jonathan b <jonathanb@github.com>
2023-10-29 19:56:23 -0500

    create logic to get user

--- routes/api.php
+++ routes/api.php
@@ -16,7 +16,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+    return response()->json(['user'=> $request->user()], 201);
 });
 
 Route::post('/login', [AuthController::class,'login'])->name('login');
